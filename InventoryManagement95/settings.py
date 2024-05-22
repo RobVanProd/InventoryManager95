836adbdccae6f4dc4ddaafbdc87ea7565854cbeb
RobVanProd <robert.Vanarsdale@gmail.com>
2024-05-21 23:42:53 -0400

    Update settings.py for deployment

--- InventoryManagement95/settings.py
+++ InventoryManagement95/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'Mdshammer1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -102,7 +102,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Default primary key field type
