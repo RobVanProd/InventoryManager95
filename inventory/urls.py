a98e0129d8fda3d2c10520f51373289acffb0d10
RobVanProd <robert.Vanarsdale@gmail.com>
2024-05-22 13:15:40 -0400

    Setup React frontend with Django

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
 from . import views, api_views
 
@@ -8,7 +9,6 @@ router.register(r'subwarehouses', api_views.SubWarehouseViewSet)
 router.register(r'items', api_views.InventoryItemViewSet)
 
 urlpatterns = [
-    path('', views.home, name='home'),
     path('add_item/', views.add_item, name='add_item'),
     path('add_warehouse/', views.add_warehouse, name='add_warehouse'),
     path('add_subwarehouse/', views.add_subwarehouse, name='add_subwarehouse'),
@@ -16,4 +16,5 @@ urlpatterns = [
     path('transfer_item/', views.transfer_item, name='transfer_item'),
     path('delete_item/<int:item_id>/', views.delete_item, name='delete_item'),
     path('api/', include(router.urls)),
+    re_path(r'^.*$', TemplateView.as_view(template_name='frontend/index.html')),  # Serve the React app
 ]
